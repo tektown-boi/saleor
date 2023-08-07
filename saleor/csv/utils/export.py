c891df4aaf98c5d29ec1fa9c019a248a0503219e
Ania Warzecha <anna.warzecha@saleor.io>
2023-08-07 18:59:43 +0200

    Remove attributes prefetch

--- saleor/csv/utils/export.py
+++ saleor/csv/utils/export.py
@@ -165,7 +165,6 @@ def export_products_in_batches(
 
     for batch_pks in queryset_in_batches(queryset):
         product_batch = Product.objects.filter(pk__in=batch_pks).prefetch_related(
-            "attributes",
             "variants",
             "collections",
             "media",
